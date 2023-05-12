37cc0571edd216311426a69ede67bba37ccc211c
Blake Burch <blakeburch@gmail.com>
2023-05-11 19:51:22 -0500

    Fix typo

--- uptime/uptime.py
+++ uptime/uptime.py
@@ -1,9 +1,9 @@
 import requests
 import os
 
-def trigger_uptime_hearbeat(url):
+def trigger_uptime_heartbeat(url):
     response = requests.post(url)
     print(response.status_code)
 
 url = os.environ.get('UPTIME_URL')
-response = trigger_uptime_hearbeat(url)
+response = trigger_uptime_heartbeat(url)
