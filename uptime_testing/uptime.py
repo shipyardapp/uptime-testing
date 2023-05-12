3a4e91e5d8486504093b5e9ee80288c2d43531be
Blake Burch <blakeburch@gmail.com>
2023-05-11 20:01:50 -0500

    Simplify trigger function

--- uptime_testing/uptime.py
+++ uptime_testing/uptime.py
@@ -3,7 +3,6 @@ import os
 
 def trigger_uptime_heartbeat(url):
     response = requests.post(url)
-    print(response.status_code)
+    return response
 
-url = os.environ.get('UPTIME_URL')
-response = trigger_uptime_heartbeat(url)
+trigger_uptime_heartbeat(url)
